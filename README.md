892d84112f8d16ce418da11cde5f64edadbc1538
Juha Airaksinen <juha.airaksinen@edu.karelia.fi>
2020-12-18 22:11:38 +0200

    basic turtle functionality

---
+++ README.md
@@ -0,0 +1,3 @@
+Install required node-modules using yarn in backend
+
+Turtle folder includes only lua scripts that the turtle uses.
--- turtle/startup.lua
+++ turtle/startup.lua
@@ -7,14 +7,21 @@ end
 if ws then
     print("> connected")
     while true do
-        local response = ws:receive()
-        if response then
-            print(response)
-            if response == "move" then
-                turtle.forward()
-            else
-                ws:send("message", "waiting")
-            end
+        ws.send("fuel_level " .. turtle.getFuelLevel())
+        ws.send("waiting")
+        local response = ws.receive()
+        if response == "forward" then
+            turtle.forward()
+        elseif(response == "back") then
+            turtle.back()
+        elseif(response == "left") then
+            turtle.turnLeft()
+        elseif(response == "right") then
+            turtle.turnRight()
+        elseif(response == "up") then
+            turtle.up()
+        elseif(response == "down") then
+            turtle.down()
         end
     end
 end
