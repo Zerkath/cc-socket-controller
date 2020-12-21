6b470cbcbdd107ca3467b32ec9d60525bb13a0ec
Zerkath <24889977+Zerkath@users.noreply.github.com>
2020-12-21 18:08:07 +0200

    added space after each enum, for new functionality

--- backend/src/turtle_actions.ts
+++ backend/src/turtle_actions.ts
@@ -1,22 +1,22 @@
 export enum move {
-  forward = "move forward",
-  back = "move back",
-  left = "move left",
-  right = "move right",
-  up = "move up",
-  down = "move down",
+  forward = "move forward ",
+  back = "move back ",
+  left = "move left ",
+  right = "move right ",
+  up = "move up ",
+  down = "move down ",
 }
 
 export enum dig {
-  forward = "dig forward",
-  up = "dig up",
-  down = "dig down",
+  forward = "dig forward ",
+  up = "dig up ",
+  down = "dig down ",
 }
 
 export enum tunnel {
-  forward = "tunnel forward",
-  up = "tunnel up",
-  down = "tunnel down",
+  forward = "tunnel forward ",
+  up = "tunnel up ",
+  down = "tunnel down ",
 }
 
 //todo make this actually nice
