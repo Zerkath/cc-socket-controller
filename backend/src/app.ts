6c2311d3b758bcda2c24847953d5744e820293e8
Juha <juha.airaksinen1@gmail.com>
2021-07-04 15:10:49 +0300

    updated dependencies

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,6 @@
 import { Server } from "ws";
 import { Turtle } from "./turtle";
-import { TurtleController } from "./turtleController";
+// import { TurtleController } from "./turtleController";
 import { dig, move, tunnel } from "./turtleAttributes";
 
 const port = 5000;
@@ -8,15 +8,15 @@ const port = 5000;
 const wss = new Server({ port: port });
 
 const turtles: Turtle[] = [];
-let tc: TurtleController;
+// let tc: TurtleController;
 
 wss.on("connection", (ws, request) => {
   const label = request.url?.slice(1);
   console.log(label + " connected");
   if (label !== "controller") {
     turtles.push(new Turtle(ws, String(label)));
-    if (tc !== undefined) tc.updateTurtles(turtles);
+    // if (tc !== undefined) tc.updateTurtles(turtles);
   } else {
-    tc = new TurtleController(ws, turtles);
+    // tc = new TurtleController(ws, turtles);
   }
 });
